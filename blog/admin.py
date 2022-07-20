669ad60d0c738dfee07ca639d6abb15ab49bd21d
Bektur <tbekturtk8@gmail.com>
2022-07-20 18:52:26 +0600

    add admin models

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-# @admin.register(Post)
-# class AdminPostsList
+@admin.register(Post)
+class AdminPostsList(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'date_pub')
+    prepopulated_fields = {"slug": ("title",)}
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from blog.models import Post
+
+from blog.models import Post, Tag
 
 # Create your views here.
 def posts_list(request):
@@ -10,3 +11,13 @@ def posts_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact = slug)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', {'tags': tags})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact = slug)
+    return render(request, 'blog/tag_detail.html', {'tag': tag})
